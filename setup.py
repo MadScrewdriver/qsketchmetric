949a7b4a6ff49b539c9bc885cafbde83daa87e1e
Franciszek Lajszczak <f.lajszczak@gmail.com>
2023-08-19 16:30:23 +0200

    Workflow commit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.6.4'
+VERSION = '1.6.5'
 DESCRIPTION = 'Parametric 2D CAD'
 LONG_DESCRIPTION = 'Parametric 2D drawings renderer using QCAD Professional'
 
