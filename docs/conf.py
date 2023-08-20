032d28462407320fd1427508ae071b5497c5fbf9
Franciszek Lajszczak <f.lajszczak@gmail.com>
2023-08-20 15:10:50 +0200

    sphinx documentation initial

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,9 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 from setup import VERSION
+
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -26,8 +31,6 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
